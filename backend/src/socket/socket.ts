336f247f98182d6edef10109845ba3dc333bc856
Prashik <gamreprashik03@gmail.com>
2025-02-23 18:59:38 +0530

    deploy commit

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors : {
-    origin : "https://chatapprash.vercel.app", //allowing request from specified origin 
+    origin : "*", //allowing request from specified origin 
     methods : ["GET","POST"], // http methods allow for corsorign request
   }
 });
